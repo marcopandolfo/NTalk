ef6cb755b097b9cbc8022b3a321aa03a9a67855e
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2021-07-22 23:16:30 -0300

    chore: fix typo

--- middleware/error.js
+++
@@ -1,7 +0,0 @@
-exports.notFound = (req, res, next) => res.status(404).render("not-found");
-
-exports.serverError = (error, req, res, next) =>
-  {
-    console.log(error);
-    return res.status(500).render("server-error", error);
-  }
